9b6c27d313be271e28d2f12896f14badd5ba6d4f
dotnet9 <1012434131@qq.com>
2021-12-13 23:52:23 +0800

    add bootstrap icons;

--- src/BlazorServer/Models/PostModel.cs
+++ src/BlazorServer/Models/PostModel.cs
@@ -13,4 +13,6 @@ public class PostModel
 	[Required]
 	[MinLength(100, ErrorMessage = "内容太短")]
 	public string? Content { get; set; }
+
+	public DateTime CreateDateTime { get; set; } = new(2021, 12, 13, 23, 40, 23);
 }
