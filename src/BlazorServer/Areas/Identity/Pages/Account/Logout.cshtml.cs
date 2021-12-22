7ea340fdcc9c3c08cbfcedfe3da7b68d403ac60d
dotnet9 <1012434131@qq.com>
2021-12-23 01:05:01 +0800

    add ASP.NET Core Identity for View;

--- src/BlazorServer/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/BlazorServer/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -24,8 +24,6 @@ public class LogoutModel : PageModel
 	{
 		await _signInManager.SignOutAsync();
 		_logger.LogInformation("User logged out.");
-		if (returnUrl != null)
-			return LocalRedirect(returnUrl);
-		return RedirectToPage();
+		return LocalRedirect(returnUrl ?? "~/Blog");
 	}
 }
